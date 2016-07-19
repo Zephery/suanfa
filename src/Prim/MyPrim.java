ef876bcdb8554a8df80642b8dc6b7e2fc28a02e3
Zephery <1570631036@qq.com>
2016-07-19 21:14:51 +0800

    updateInsertSort

--- src/Prim/MyPrim.java
+++ src/Prim/MyPrim.java
@@ -1,54 +1,129 @@
 package Prim;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by Zephery on 2016/7/19.
  */
 public class MyPrim {
-    private static int MAX = 65535;
+    private char[] mVexs;
+    private int[][] mMatrix;
+    private static final int INF = Integer.MAX_VALUE;
 
-    private static void myprim(int[][] graph, int n) {
-        char[] c = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'E', 'F'};
-        int[] lowcost = new int[n];
-        int[] mst = new int[n];
-        int i, j, min, minid, sum = 0;
-        for (i = 1; i < n; i++) {
-            lowcost[i] = graph[0][1];
-            mst[i] = 0;
-        }
-        for (i = 1; i < n; i++) {
-            min = MAX;
-            minid = 0;
-            for (j = 1; j < n; j++) {
-                if (lowcost[j] < min && lowcost[j] != 0) {
-                    min = lowcost[j];
-                    minid = j;
-                }
+    private int readInt() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    private char readChar() {
+        char ch = '0';
+        do {
+            try {
+                ch = (char) System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            System.out.println(c[minid] + "到" + c[minid] + "权值" + min);
+        } while (!((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')));
+        return ch;
+    }
+
+    private int getPosition(char ch) {
+        for (int i = 0; i < mVexs.length; i++)
+            if (mVexs[i] == ch)
+                return i;
+
+        return -1;
+    }
+
+    public MyPrim() {
+        System.out.printf("Please input vertex number");
+        int vlen = readInt();
+        System.out.printf("Please input edge number:");
+        int elen = readInt();
+        if (vlen < 1 || elen < 1 || (elen > (vlen * (vlen - 1)))) {
+            System.out.printf("input error:invalid parameters!\n");
+            return;
+        }
+        mVexs = new char[vlen];
+        for (int i = 0; i < mVexs.length; i++) {
+            System.out.printf("vertex(%d)", i);
+            mVexs[i] = readChar();
+        }
+        mMatrix = new int[vlen][vlen];
+        for (int i = 0; i < vlen; i++) {
+            for (int j = 0; j < vlen; j++) {
+                if (i == j)
+                    mMatrix[i][j] = 0;
+                else
+                    mMatrix[i][j] = INF;
+            }
+        }
+        for (int i = 0; i < elen; i++) {
+            System.out.printf("edge(%d):", i);
+            char c1 = readChar();
+            char c2 = readChar();
+            int weight = readInt();
+            int p1 = getPosition(c1);
+            int p2 = getPosition(c2);
+            if (p1 == -1 || p2 == -1) {
+                System.out.printf("input error:invalid edge!\n");
+                return;
+            }
+            mMatrix[p1][p2] = weight;
+            mMatrix[p2][p1] = weight;
+        }
+
+    }
+
+    public void prim(int start) {
+        int num = mVexs.length;
+        int index = 0;
+        char[] prims = new char[num];
+        int[] weights = new int[num];
+        prims[index++] = mVexs[start];
+        for (int i = 0; i < num; i++)
+            weights[i] = mMatrix[start][i];
+        weights[start] = 0;
+        for (int i = 0; i < num; i++) {
+            if (start == i)
+                continue;
+            int j = 0;
+            int k = 0;
+            int min = INF;
+            while (j < num) {
+                if (weights[j] != 0 && weights[j] < min) {
+                    min = weights[j];
+                    k = j;
+                }
+                j++;
+            }
+            prims[index++] = mVexs[k];
+            weights[k] = 0;
+            for (j = 0; j < num; j++) {
+                if (weights[j] != 0 && mMatrix[k][j] < weights[j])
+                    weights[j] = mMatrix[k][j];
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < index; i++) {
+            int min = INF;
+            int n = getPosition(prims[i]);
+            for (int j = 0; j < i; j++) {
+                int m = getPosition(prims[j]);
+                if (mMatrix[m][n] < min)
+                    min = mMatrix[m][n];
+            }
             sum += min;
-            lowcost[minid] = 0;
-            for (j = 1; j < n; j++) {
-                if (graph[minid][j] < lowcost[j]) {
-                    lowcost[j] = graph[minid][j];
-                    mst[j] = minid;
-                }
-            }
         }
-        System.out.println("sum:" + sum);
+        System.out.printf("PRIM(%c)=%d:", mVexs[start], sum);
+        for (int i = 0; i < index; i++)
+            System.out.printf("%c", prims[i]);
+        System.out.printf("\n");
     }
 
     public static void main(String args[]) {
-        int[][] map = new int[][]{
-                {0, 10, MAX, MAX, MAX, 11, MAX, MAX, MAX},
-                {10, 0, 18, MAX, MAX, MAX, 16, MAX, 12},
-                {MAX, MAX, 0, 22, MAX, MAX, MAX, MAX, 8},
-                {MAX, MAX, 22, 0, 20, MAX, MAX, 16, 21},
-                {MAX, MAX, MAX, 20, 0, 26, MAX, 7, MAX},
-                {11, MAX, MAX, MAX, 26, 0, 17, MAX, MAX},
-                {MAX, 16, MAX, MAX, MAX, 17, 0, 19, MAX},
-                {MAX, MAX, MAX, 16, 7, MAX, 19, 0, MAX},
-                {MAX, 12, 8, 21, MAX, MAX, MAX, MAX, 0}
-        };
-        myprim(map, map.length);
+        MyPrim myPrim = new MyPrim();
+        myPrim.prim(0);
     }
 }
--- src/ShellSort/ShellSort.java
+++ src/ShellSort/ShellSort.java
@@ -6,6 +6,57 @@ import java.util.Scanner;
  * Created by Zephery on 2016/7/18.
  */
 public class ShellSort {
+    private static void shellsort(int a[], int n) {
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = 0; i < gap; i++) {
+                for (int j = i + gap; j < n; j += gap) {
+                    if (a[j] < a[j - gap]) {
+                        int tmp = a[j];
+                        int k = j - gap;
+                        while (k >= 0 && a[k] > tmp) {
+                            a[k + gap] = a[k];
+                            k -= gap;
+                        }
+                        a[k + gap] = tmp;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void groupSort(int[] a, int n, int i, int gap) {
+
+        for (int j = i + gap; j < n; j += gap) {
+
+            // 如果a[j] < a[j-gap]，则寻找a[j]位置，并将后面数据的位置都后移。
+            if (a[j] < a[j - gap]) {
+
+                int tmp = a[j];
+                int k = j - gap;
+                while (k >= 0 && a[k] > tmp) {
+                    a[k + gap] = a[k];
+                    k -= gap;
+                }
+                a[k + gap] = tmp;
+            }
+        }
+    }
+
+    /**
+     * 希尔排序
+     * <p>
+     * 参数说明：
+     * a -- 待排序的数组
+     * n -- 数组的长度
+     */
+    public static void shellSort2(int[] a, int n) {
+        // gap为步长，每次减为原来的一半。
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            // 共gap个组，对每一组都执行直接插入排序
+            for (int i = 0; i < gap; i++)
+                groupSort(a, n, i, gap);
+        }
+    }
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
@@ -15,6 +66,13 @@ public class ShellSort {
         int num[] = new int[stringArray.length];
         for (int i = 0; i < stringArray.length; i++) {
             num[i] = Integer.parseInt(stringArray[i]);
+        }
+        System.out.println("Please input n:");
+        int n = sc.nextInt();
+        shellsort(num,num.length);
+        //shellSort2(num, n);
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i] + " ");
         }
     }
 }
